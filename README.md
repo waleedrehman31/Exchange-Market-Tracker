740f360e5a8ae53ba0a0b0a94977120b36a69869
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-08-24 17:40:04 +0500

    Adding Currency and working on Front-end

--- README.md
+++ README.md
@@ -26,6 +26,8 @@
 - Chartjs CDN
   `<script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>`
 - Tailwind CSS and DailyUI
-  `<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+`
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@1.13.2/dist/full.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@1.13.2/dist/themes.css" rel="stylesheet" type="text/css" />`
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@1.13.2/dist/themes.css" rel="stylesheet" type="text/css" />
+    `
--- app.js
+++ app.js
@@ -1,40 +1,30 @@
-const API_KEY = config.My_ACCESS_KEY;
-const API_URI = `http://api.marketstack.com/v1/exchanges?access_key=${API_KEY}`;
+const CURRENCY_API_KEY = config.CURRENCY_ACCESS_KEY;
+const CURRENCY_API_URI = `http://api.marketstack.com/v1/currencies?access_key=${CURRENCY_API_KEY}`;
+const STOCK_MARKET_API_KEY = config.STOCK_MARKET_API_KEY;
+const STOCK_MARKET_API_URI = `https://www.alphavantage.co/query?function=REAL_GDP&interval=annual&apikey=${STOCK_MARKET_API_KEY}`;
+const currencyTable = document.getElementById("currency-table");
 
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
+async function getCurrency() {
+  const data = await fetch(CURRENCY_API_URI);
+  const JSON_DATA = await data.json();
+  JSON_DATA.data.forEach((currency, sNO = 1) => {
+    // console.log(currency);
+    currencyTable.innerHTML += `
+    <tr class="hover">
+              <th>${++sNO}</th>
+              <td>${currency.name}</td>
+              <td>${currency.code}</td>
+              <td>${currency.symbol}</td>
+            </tr>
+    `;
+  });
+}
+
+async function getMarket() {
+  const data = await fetch(STOCK_MARKET_API_URI);
+  const JSON_DATA = await data.json();
+  console.log(JSON_DATA);
+}
+
+getCurrency();
+getMarket();
